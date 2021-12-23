ea52b8874ef1fe22e400110b3f0b9466428226e0
Daniel <djp21795@yahoo.com>
2021-12-22 22:22:57 -0500

    Add more solutions

---
+++ 20211222 - Sample Questions/143. Reorder List - Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head==null||head.next==null) return;
+        Deque<ListNode> stack = new ArrayDeque<ListNode>();
+        ListNode ptr=head;
+        while (ptr!=null) {
+            stack.push(ptr); ptr=ptr.next;
+        }
+        int cnt=(stack.size()-1)/2;
+        ptr=head;
+        while (cnt-->0) {
+            ListNode top = stack.pop();
+            ListNode tmp = ptr.next;
+            ptr.next=top;
+            top.next=tmp;
+            ptr=tmp;
+        }
+        stack.pop().next=null;
+    }
+}
+//TODO
