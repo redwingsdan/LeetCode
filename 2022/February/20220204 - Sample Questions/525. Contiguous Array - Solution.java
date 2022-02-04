6cf54be3ffe4dce8a8e6b3f5ff0c5e6eb3b8667d
Daniel Peterson <djp21795@yahoo.com>
2022-02-04 11:36:20 -0500

    Update 525. Contiguous Array - Solution.java

--- 2022/February/20220204 - Sample Questions/525. Contiguous Array - Solution.java
+++ 2022/February/20220204 - Sample Questions/525. Contiguous Array - Solution.java
@@ -1,5 +1,19 @@
 class Solution {
-    public int findMaxLength(int[] nums) {
-        
+    public int findMaxLength(int[] nums) {       
+        int[] totalZeros = new int[2 * nums.length + 1];
+        Arrays.fill(totalZeros, -2);
+        totalZeros[nums.length] = -1;
+        int longest = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 0 ? -1 : 1);
+            if (totalZeros[count + nums.length] >= -1) {
+                longest = Math.max(longest, i - totalZeros[count + nums.length]);
+            } else {
+                totalZeros[count + nums.length] = i;
+            }
+
+        }
+        return longest;
     }
 }
